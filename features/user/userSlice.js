005d8b525751dc5554c30089510e59a9b8025797
codingJourneyFromUnemployment <kwg6qlyv@hotmail.com>
2023-08-11 21:17:49 +0800

    asyncThunk

--- features/user/userSlice.js
+++ features/user/userSlice.js
@@ -8,12 +8,14 @@ const _initialState = {
   error: '',
 };
 // createAsyncThunk模块用于处理异步请求，第一个参数为action的type，第二个参数为异步请求的回调函数，这里是在定义派发action时的回调函数
+// 在使用createAsyncThunk时，会自动创建三个action type，分别为pending, fulfilled, rejected，分别对应异步请求开始触发，异步请求成功触发，异步请求失败触发
 const fetchUser = createAsyncThunk("user/fetchUser", async () => {
   const response = await axios.get('https://jsonplaceholder.typicode.com/users');
   const userIDs = response.data.map(user => user.id);
   return userIDs;
 });
 // createSlice模块用于创建reducer，第一个参数为reducer的名称，第二个参数为reducer的初始状态，第三个参数为reducer的回调函数。builder.addCase用于处理action,在不同的action.type下执行不同的回调函数来更新state
+// reducers中一般用于处理那些由这个slice自身生成的action，而extraReducers中一般用于处理那些不属于当前slice定义的actions。由于pending, fulfilled, rejected三个action type是createAsyncThunk自动生成的，所以需要在extraReducers中处理。
 const userSlice = createSlice({
   name: 'user',
   initialState: _initialState,
